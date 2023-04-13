3a01abcbf82b7e185d2c3dcb953f9338af5cc465
Edchel Stephen Nini <edchelstephens@gmail.com>
2023-04-13 22:53:20 +0800

    settings: ALLOWED HOSTS.

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
 
 # Application definition
--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -1,7 +1,3 @@
 from portfolio.settings.base import *  # noqa
 
 DEBUG = False
-
-ALLOWED_HOSTS = [
-    "djangodeploy-sandbox-env1.eba-abiastx8.ap-southeast-2.elasticbeanstalk.com",
-]
