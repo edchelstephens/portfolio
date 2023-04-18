b9d36e5ae35fedda5c9e4ca7307c284482ad0244
Edchel Stephen Nini <edchelstephens@gmail.com>
2023-04-18 12:14:58 +0800

    settings: logging config to file.

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -1,3 +1,32 @@
 from portfolio.settings.base import *  # noqa
 
 DEBUG = False
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "django.log",
+        },
+        "contact": {
+            "class": "logging.FileHandler",
+            "filename": "contact.log",
+            "level": "DEBUG",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+        "contact": {
+            "level": "DEBUG",
+            "handlers": ["contact"],
+        },
+    },
+}
