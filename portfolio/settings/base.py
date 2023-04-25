354f7f3f943f078a5632a02fef83283e6d4fab78
Edchel Stephen Nini <edchelstephens@gmail.com>
2023-04-25 19:27:25 +0800

    chore: restore static on static root.

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = env.str("DJANGO_STATIC_ROOT", os.path.join(BASE_DIR, "staticfiles/"))
+STATIC_ROOT = env.str("DJANGO_STATIC_ROOT", os.path.join(BASE_DIR, "static/"))
 
 
 MEDIA_URL = "media/"
