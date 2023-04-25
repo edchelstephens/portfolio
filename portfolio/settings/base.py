85e7b63402e1df2c45008d6c0e9cfd56c37e3c37
Edchel Stephen Nini <edchelstephens@gmail.com>
2023-04-25 18:56:43 +0800

    settings: use postgresql driver.

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -110,7 +110,7 @@ if env.bool("USES_POSTGRES_DB", False):
 if "RDS_HOSTNAME" in os.environ:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.mysql",
+            "ENGINE": "django.db.backends.postgresql",
             "NAME": os.environ["RDS_DB_NAME"],
             "USER": os.environ["RDS_USERNAME"],
             "PASSWORD": os.environ["RDS_PASSWORD"],
